07cda15f6076d81c8f3c975ddd74bde7d95ee8e9
kyle <krwodzicki@gmail.com>
2019-02-23 15:34:45 -0600

    Removes extra files
    This version removes a bunch of files that are no longer used

--- pyBackup/icons.py
+++
@@ -1,5 +0,0 @@
-import os;
-main   = os.path.dirname( os.path.realpath(__file__) );
-main   = os.path.join(main, 'data');
-images = [os.path.join(main, f) for f in os.listdir(main)]
-main   = images[-1];
