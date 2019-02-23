4746c99bdda7b2a83ea1aed025965b04c2a5b102
kyle <krwodzicki@gmail.com>
2019-02-23 15:47:38 -0600

    Includes package data

--- setup.py
+++ setup.py
@@ -4,14 +4,16 @@ import setuptools
 
 
 setuptools.setup(
-  name             = "pyBackup",
-  description      = "A program for OS X Time Machine like backups",
-  url              = "https://github.com/kwodzicki/pyBackup",
-  author           = "Kyle R. Wodzicki",
-  author_email     = "krwodzicki@gmail.com",
-  version          = "0.0.9",
-  packages         = setuptools.find_packages(),
-  install_requires = ['PyQt5', 'python-crontab'],
-  scripts          = ['bin/pyBackup'],
-  zip_safe         = False
+  name                 = "pyBackup",
+  description          = "A program for OS X Time Machine like backups",
+  url                  = "https://github.com/kwodzicki/pyBackup",
+  author               = "Kyle R. Wodzicki",
+  author_email         = "krwodzicki@gmail.com",
+  version              = "0.0.10",
+  packages             = setuptools.find_packages(),
+  install_requires     = ['PyQt5', 'python-crontab'],
+  scripts              = ['bin/pyBackup'],
+  package_data         = {'pyBackup' : ['config.json']},
+  include_package_data = True,
+  zip_safe             = False
 );
